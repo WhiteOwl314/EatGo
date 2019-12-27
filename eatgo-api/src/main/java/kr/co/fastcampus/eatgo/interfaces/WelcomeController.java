fcd39e05b20af4468ebd278bbbd8b3ec32246de8
WhiteOwl314 <parksj914@naver.com>
2019-12-27 12:18:20 +0900

    create HelloWorld Application

---
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/WelcomeController.java
@@ -0,0 +1,13 @@
+package kr.co.fastcampus.eatgo.interfaces;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //REST API 쓰기 위함
+public class WelcomeController {
+
+    @GetMapping("/") //http 접속을 위한 네가지 메소드 중 하나 , /는 접속하고 바로
+    public String hello() {
+        return "Hello, world!";
+    }
+}
